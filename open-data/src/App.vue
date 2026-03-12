fc75fc2aa8f775306fd1a6948aab1f3e5fc098b7
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-03-12 12:27:49 -0400

    a

---
+++ open-data/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+const api = "https://data.cityofnewyork.us/resource/i7jb-7jku.json"
+onMounted(async () => {
+  try {
+    const response = await fetch(api);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+)
+</script>
+
+<template>
+  <HelloWorld />
+</template>
---
+++ open-data/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const prop = defineProps({
+  data: {
+    type: Object, 
+    required: true},
+  id: {
+    type: Number,
+    required: true
+  }
+  })
+
+</script>
+
+
+<template>
+
+</template>
