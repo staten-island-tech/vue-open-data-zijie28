fee48ebbc020896ed1a69f6b1dcbf4028ca69753
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-03-16 12:32:35 -0400

    how does one have inf recursion

--- open-data/src/App.vue
+++ open-data/src/App.vue
@@ -1,24 +1,45 @@
 <script setup>
-import { onMounted } from 'vue';
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import BarChart from './components/BarChart.vue'
+import { Bar } from 'vue-chartjs';
 const api = "https://data.cityofnewyork.us/resource/i7jb-7jku.json"
+let labels = ref([]);
+let data = ref([]);
 onMounted(async () => {
   try {
     const response = await fetch(api);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
     const result = await response.json();
-    console.log(result);
+    const count = counter(result);
+    console.log(count)
+    count.forEach((value, key) => {
+      console.log(value)
+      console.log(key)
+      labels.value.push(key);
+      data.value.push(value);
+    })
+
   } catch (error) {
     console.error(error.message);
   }
+  
+  
+}
+)
+
+function counter(data) {
+    const counts = new Map();
+    for (const item of data) {
+        counts.set(item['location_type'], (counts.get(item['location_type']) || 0) + 1);
+    }
+    return counts;
 }
 
-)
+
 </script>
 
 <template>
-  <HelloWorld />
+<BarChart :data="data" :lables="labels"></BarChart>
 </template>
---
+++ open-data/src/components/BarChart.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+const prop = defineProps({
+  data: {
+    type: Object, 
+    required: true
+  },
+  lables: {
+    type: Object,
+    required: true
+  }
+  })
+
+const lable = prop.lables;
+const data = {
+  lables: lable,
+  datasets: [{
+    lable: 'Public Bathrooms',
+    data: prop.data,
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+    ],
+    borderWidth: 1
+  }]
+}
+const options = {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
+</script>
+
+
+<template>
+<Bar id="chart" :options="options" :data="data"/>
+</template>
