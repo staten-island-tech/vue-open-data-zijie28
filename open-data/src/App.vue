034eb5cec9a62956c2113a1c3cf7a42218b7eb09
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-03-27 16:51:22 +0000

    nvm im finished

--- open-data/src/App.vue
+++ open-data/src/App.vue
@@ -5,6 +5,5 @@
 <template>
 <RouterLink to="/main" >Location of Bathrooms</RouterLink>
 <RouterLink to="/status">Status of Bathrooms</RouterLink>
-<RouterLink to="/op">Operator of Bathrooms</RouterLink>
 <RouterView></RouterView>
 </template> 
--- open-data/src/router.js
+++ open-data/src/router.js
@@ -3,13 +3,11 @@ import { createWebHistory, createRouter } from 'vue-router';
 import App from './App.vue';
 import StatusPage from './views/StatusPage.vue';
 import HomePage from './views/HomePage.vue';
-import OpPage from './views/OpPage.vue';
 
 
 const routes = [
   { name: "status", path: '/status', component: StatusPage },
   { name: "main", path: '/main', component: HomePage },
-  { name: "main", path: '/op', component: OpPage },
 ];
 
 export const router = createRouter({
